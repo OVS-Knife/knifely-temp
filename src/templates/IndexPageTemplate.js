f0830d3a5057ae65e66e79bd638b42faed76a3d0
BrendanGlancy <bglance68@gmail.com>
2023-07-21 09:18:53 -0400

    sorry linnie

--- src/templates/IndexPageTemplate.js
+++ src/templates/IndexPageTemplate.js
@@ -17,7 +17,7 @@ const IndexPageTemplate = ({
       {/* Header */}
       <div className="relative bg-blue-600 overflow-hidden">
         <div className="max-w-7xl mx-auto">
-          <div className="relative z-10 pt-24 pb-12 bg-blue-600 sm:pt-28 sm:pb-14 md:pt-32 md:pb-16 lg:w-1/2 lg:pt-44 lg:pb-24 xl:pt-48 xl:pb-32">
+          <div className="relative z-10 pt-16 pb-8 bg-blue-600 sm:pt-20 sm:pb-10 md:pt-24 md:pb-12 lg:w-1/2 lg:pt-44 lg:pb-24 xl:pt-48 xl:pb-32">
             <svg
               className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-blue-600 transform translate-x-1/2"
               fill="currentColor"
@@ -30,13 +30,13 @@ const IndexPageTemplate = ({
 
             <div className="relative z-20 mx-auto lg:max-w-7xl px-4 sm:px-6 lg:px-8">
               <div className="text-center max-w-md mx-auto lg:max-w-lg lg:mx-0 lg:text-left">
-                <h1 className="text-3xl font-bold text-white sm:text-4xl lg:text-5xl">
+                <h1 className="text-2xl font-bold text-white sm:text-3xl lg:text-5xl">
                   {heading}
                 </h1>
-                <p className="mt-2 text-blue-100 sm:mt-4 lg:mt-6 lg:text-xl">
+                <p className="mt-2 text-blue-100 sm:mt-3 lg:mt-4 lg:text-xl">
                   {subheading}
                 </p>
-                <PrimaryButton to="/contact" className="mt-6 lg:mt-10 text-blue-100">
+                <PrimaryButton to="/contact" className="mt-4 lg:mt-10 text-blue-100">
                   Contact us
                 </PrimaryButton>
               </div>
@@ -46,7 +46,7 @@ const IndexPageTemplate = ({
 
         <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
           <Image
-            className="h-56 w-full object-cover sm:h-72 md:h-92 lg:w-full lg:h-full"
+            className="h-48 w-full object-cover sm:h-64 md:h-80 lg:w-full lg:h-full"
             image={image}
             alt=""
           />
@@ -55,13 +55,13 @@ const IndexPageTemplate = ({
 
       {/* Featured projects */}
       <Container>
-        <div className="flex justify-between items-baseline">
+        <div className="flex flex-col sm:flex-row justify-between items-baseline">
           <SectionHeading>Check out our Knife Sharpening Blog</SectionHeading>
-          <Link className="text-blue-600 underline hover:text-blue-800" to="/projects">
+          <Link className="text-blue-600 underline hover:text-blue-800 mt-2 sm:mt-0" to="/projects">
             View all
           </Link>
         </div>
-        <div className="mt-8">
+        <div className="mt-6">
           <CardSet posts={posts} />
         </div>
       </Container>
@@ -72,7 +72,7 @@ const IndexPageTemplate = ({
         <p className="mt-2 text-gray-600 dark:text-gray-300">
           {about.description}
         </p>
-        <SecondaryButton to={about.button.url} className="mt-6 lg:mt-10">
+        <SecondaryButton to={about.button.url} className="mt-4 lg:mt-8">
           {about.button.label}
         </SecondaryButton>
       </TextImageSplit>
@@ -80,4 +80,4 @@ const IndexPageTemplate = ({
   )
 }
 
-export default IndexPageTemplate
+export default IndexPageTemplate;
